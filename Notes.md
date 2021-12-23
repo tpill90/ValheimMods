d9f26b69da0ffee9769acca6167ca61d3be5cf8b
Tim Pilius <tpilius@gmail.com>
2021-12-23 15:01:15 -0500

    Adding a script that installs required dependencies, and updating documentation to include running this required script.

--- Notes.md
+++
@@ -1,7 +0,0 @@
-Valheim Game logic dll - "..\Valheim\valheim_Data\Managed\publicized_assemblies\assembly_valheim_publicized.dll"
-
-https://valheim-modding.github.io/Jotunn/tutorials/overview.html
-
-https://github.com/Valheim-Modding/Valheim.DisplayBepInExInfo
-
-https://github.com/Valheim-Modding/JotunnModStub
--- scripts/Download-Dependencies.ps1
+++ scripts/Download-Dependencies.ps1
@@ -1,3 +1,22 @@
+function Ensure-BepinExIsInstalled
+{
+    if(Test-Path "$valheimInstallDir\BepInEx")
+    {
+        Write-Host "BepInEx already installed.  Skipping"
+    }
+    # Downloading BepinEx
+    $package = $packageList | Where-Object { $_.Name -eq "BepInExPack_Valheim" } | Select -First 1
+    $version = $package.Versions | Select -First 1
+
+    Write-Host "Downloading BepInEx..."
+    Invoke-WebRequest -Uri $version.download_url -OutFile "$Env:TEMP\$($version.name).zip"
+    Write-Host "Extracting..."
+    Expand-Archive -LiteralPath "$Env:TEMP\$($version.name).zip" -DestinationPath $Env:TEMP -Force
+    Write-Host "Copying"
+    Copy-Item "$Env:TEMP\$($version.name)\*" -Destination $valheimInstallDir -Recurse -Force
+}
+
+
 Push-Location $PSScriptRoot
 
 $ErrorActionPreference = "Stop"
@@ -12,23 +31,11 @@ if(-Not(Test-Path $valheimInstallDir))
     Write-Error "Cannot detect Valheim install at $valheimInstallDir.  Stopping..."
 }
 
-
-Write-Host "Getting package list"
+Write-Host "Querying Thunderstore for package list..."
 $packageList = Invoke-RestMethod "https://valheim.thunderstore.io/api/v1/package/"
 $packageList = $packageList | Where-Object { $_.is_deprecated -eq $false }
 
-
-# Downloading BepinEx
-$package = $packageList | Where-Object { $_.Name -eq "BepInExPack_Valheim" } | Select -First 1
-$version = $package.Versions | Select -First 1
-
-#TODO color + more detail
-Write-Host "Downloading..."
-Invoke-WebRequest -Uri $version.download_url -OutFile "$Env:TEMP\$($version.name).zip"
-Write-Host "Extracting..."
-Expand-Archive -LiteralPath "$Env:TEMP\$($version.name).zip" -DestinationPath $Env:TEMP -Force
-Write-Host "Copying"
-#Copy-Item "$Env:TEMP\$($version.name)\*" -Destination $valheimInstallDir -Recurse -Force
+Ensure-BepinExIsInstalled
 
 # Installing mod dependencies
 $package = $packageList | Where-Object { $_.full_name -eq "pipakin-SkillInjector" } | Select -First 1
