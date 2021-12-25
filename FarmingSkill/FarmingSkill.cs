185024ee2bcb7d5065afb9ec91b92eb294a2fe92
Tim Pilius <tpilius@gmail.com>
2021-12-25 17:15:10 -0500

    Reformatting source code structure

--- FarmingSkill/FarmingSkill.cs
+++ FarmingSkill/FarmingSkill.cs
@@ -1,88 +1,81 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using HarmonyLib;
 using Pipakin.SkillInjectorMod;
 
 namespace FarmingSkill
 {
-    [BepInPlugin("tpill90.FarmingSkill", "FarmingSkill", "0.0.4")]
+    [BepInPlugin("tpill90.FarmingSkill", "FarmingSkill", "0.0.5")]
     [BepInDependency("com.pipakin.SkillInjectorMod")]
     public class FarmingSkill : BaseUnityPlugin
     {
-        const string MOD_ID = "tpill90.FarmingSkill";
-        private readonly Harmony harmony = new Harmony(MOD_ID);
-
-        // Hopefully this doesn't conflict.
-        public const int SKILL_TYPE = 287;
+        public static int SKILL_TYPE = 287;
 
         void Awake()
         {
             Logger.LogInfo("Loading FarmingSkill Mod...");
 
-            harmony.PatchAll(typeof(ReduceCultivatorStaminaCost));
-            harmony.PatchAll(typeof(ConsumeResources));
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 
             SkillInjector.RegisterNewSkill(SKILL_TYPE, "Farming", "Affects cultivator stamina use", 1.0f, 
                 TextureLoader.LoadCustomTexture("FarmingSkill.cultivator.png"), Skills.SkillType.Run);
         }
+    }
 
-        //TODO try to format these harmony patches differently, hard to read like this
+    [HarmonyPatch]
+    public static class FarmingSkillPatches
+    {
+        [HarmonyPostfix]
         [HarmonyPatch(typeof(Player), nameof(Player.ConsumeResources))]
-        public static class ConsumeResources
+        public static void IncreaseSkillExperience(Player __instance)
         {
-            [HarmonyPostfix]
-            public static void PostFix(Player __instance, Piece.Requirement[] requirements, int qualityLevel)
+            if (__instance.GetRightItem() == null)
             {
-                if (__instance.GetRightItem() == null)
-                {
-                    return;
-                }
+                return;
+            }
 
-                string usedItemName = __instance.GetRightItem().m_shared.m_name;
-                if (usedItemName != "$item_cultivator")
-                {
-                    return;
-                }
+            string usedItemName = __instance.GetRightItem().m_shared.m_name;
+            if (usedItemName != "$item_cultivator")
+            {
+                return;
+            }
 
-                var playerIsRested = __instance.m_seman.HaveStatusEffect("Rested");
-                if (playerIsRested)
-                {
-                    // Being rested grants a 50% XP bonus
-                    __instance.RaiseSkill((Skills.SkillType)SKILL_TYPE, 1.5f);
-                }
-                else
-                {
-                    __instance.RaiseSkill((Skills.SkillType)SKILL_TYPE, 1);
-                }
+            var playerIsRested = __instance.m_seman.HaveStatusEffect("Rested");
+            if (playerIsRested)
+            {
+                // Being rested grants a 50% XP bonus
+                __instance.RaiseSkill((Skills.SkillType)FarmingSkill.SKILL_TYPE, 1.5f);
+            }
+            else
+            {
+                __instance.RaiseSkill((Skills.SkillType)FarmingSkill.SKILL_TYPE, 1);
             }
         }
 
+        [HarmonyPrefix]
         [HarmonyPatch(typeof(Player), nameof(Player.UseStamina))]
-        public static class ReduceCultivatorStaminaCost
+        public static void ReduceCultivatorStaminaCost(ref float v, Player __instance)
         {
-            [HarmonyPrefix]
-            public static void Prefix(ref float v, Player __instance)
+            if (__instance.GetRightItem() == null)
             {
-                if (__instance.GetRightItem() == null)
-                {
-                    return;
-                }
-                    
-                string usedItemName = __instance.GetRightItem().m_shared.m_name;
-                if (usedItemName != "$item_cultivator")
-                {
-                    return;
-                }
-
-                // Player has likely installed the mod, but not placed any plants yet, so the skill has not been initialized
-                if (!__instance.GetSkills().m_skillData.ContainsKey((Skills.SkillType) SKILL_TYPE))
-                {
-                    return;
-                }
-
-                var currentSkill = __instance.GetSkills().m_skillData[(Skills.SkillType)SKILL_TYPE];
-                // Reduces stamina by a maximum of 66% at level 100
-                v = v * (1 - (currentSkill.m_level * ((2f / 3f) / 100)));
+                return;
             }
+
+            string usedItemName = __instance.GetRightItem().m_shared.m_name;
+            if (usedItemName != "$item_cultivator")
+            {
+                return;
+            }
+
+            // Player has likely installed the mod, but not placed any plants yet, so the skill has not been initialized
+            if (!__instance.GetSkills().m_skillData.ContainsKey((Skills.SkillType)FarmingSkill.SKILL_TYPE))
+            {
+                return;
+            }
+
+            var currentSkill = __instance.GetSkills().m_skillData[(Skills.SkillType)FarmingSkill.SKILL_TYPE];
+            // Reduces stamina by a maximum of 66% at level 100
+            v = v * (1 - (currentSkill.m_level * ((2f / 3f) / 100)));
         }
     }
 }
